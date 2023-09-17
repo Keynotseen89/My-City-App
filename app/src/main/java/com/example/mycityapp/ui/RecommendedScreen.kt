e79ffbf167b9ed48bae7988ecea48a4b1606b62a
Keynotseen89 <ksintora@yahoo.com>
2023-09-16 19:34:13 -0700

    Created Previews for the Screen layouts

--- app/src/main/java/com/example/mycityapp/ui/RecommendedScreen.kt
+++ app/src/main/java/com/example/mycityapp/ui/RecommendedScreen.kt
@@ -1,4 +1,7 @@
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -6,11 +9,14 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,7 +26,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -31,9 +40,7 @@ import com.example.mycityapp.data.RecommendationDataProvider
 import com.example.mycityapp.model.Recommendation
 import com.example.mycityapp.ui.theme.MyCityAppTheme
 
-/**
- * TODO: Finish up RecommendedList
- */
+
 @Composable
 private fun RecommendedList(
     recommendation: List<Recommendation>,
@@ -131,6 +138,99 @@ private fun RecommendedListImageItem(
     }
 }
 
+@Composable
+private fun RecommendedListAndDetail(
+    recommended: List<Recommendation>,
+    selectedRecommendation: Recommendation,
+    onClick: (Recommendation) -> Unit,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = modifier
+    ){
+        RecommendedList(
+            recommendation = recommended,
+            modifier = Modifier.weight(2f),
+            onClick = onClick
+        )
+        val activity = (LocalContext.current as Activity)
+        RecommendedDetail(
+            selectedRecommendation = selectedRecommendation,
+            modifier = Modifier.weight(3f),
+            onBackPressed = { activity.finish() }
+        )
+    }
+}
+
+@Composable
+private fun RecommendedDetail(
+    selectedRecommendation: Recommendation,
+    onBackPressed: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    BackHandler {
+        onBackPressed()
+    }
+    val scrollState = rememberScrollState()
+
+    Box(
+        modifier = modifier
+            .verticalScroll(state = scrollState)
+    ){
+        Column{
+            Box{
+                Box{
+                    Image(
+                        painter = painterResource(selectedRecommendation.recommendedImageBanner),
+                        contentDescription = null,
+                        alignment = Alignment.TopCenter,
+                        contentScale =  ContentScale.FillWidth,
+                    )
+                }
+                Column(
+                    Modifier
+                        .align(Alignment.BottomStart)
+                        .fillMaxWidth()
+                        .background(
+                            Brush.verticalGradient(
+                                listOf(Color.Transparent, MaterialTheme.colorScheme.scrim),
+                                0f,
+                                400f
+                            )
+                        )
+                ){
+                    Text(
+                        text = stringResource(selectedRecommendation.titleResourcesId),
+                        style = MaterialTheme.typography.headlineLarge,
+                        color = MaterialTheme.colorScheme.inverseOnSurface,
+                        modifier = Modifier
+                            .padding(horizontal = dimensionResource(R.dimen.padding_small))
+                    )
+                    Row(
+                        modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                    ){
+                        /*Text(
+                            text = stringResource(R.),
+                            style = ,
+                            color = ,
+                        )
+                        Spacer(Modifier.weight(1f))
+                        Text()*/
+                    }
+                }
+            } //Finish up the box in a while
+            Text(
+                text = stringResource(selectedRecommendation.recommendedDetails),
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(
+                    vertical = dimensionResource(R.dimen.padding_detail_content_vertical),
+                    horizontal = dimensionResource(R.dimen.padding_detail_content_horizontal)
+                )
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
 fun RecommendedListPreview(){
@@ -139,6 +239,36 @@ fun RecommendedListPreview(){
             RecommendedList(
                 recommendation = RecommendationDataProvider.getRecommendation(),
                 onClick = {})
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RecommendedDetailPreview(){
+    MyCityAppTheme {
+        Surface {
+            RecommendedDetail(
+                selectedRecommendation = RecommendationDataProvider.getRecommendation()[0],
+                onBackPressed = {}
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RecommendedListAndDetailPreview(){
+    MyCityAppTheme {
+        Surface {
+            RecommendedListAndDetail(
+                recommended = RecommendationDataProvider.getRecommendation(),
+                selectedRecommendation = RecommendationDataProvider.getRecommendation().getOrElse(0){
+                    RecommendationDataProvider.defaultRecommendation
+                },
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
