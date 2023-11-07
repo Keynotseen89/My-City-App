0cb5dfa54d97c9587abf15d590b5777709305a47
Keynotseen89 <ksintora@yahoo.com>
2023-11-06 16:28:31 -0800

    Updated theme files

--- app/src/main/java/com/example/mycityapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/mycityapp/ui/theme/Type.kt
@@ -2,33 +2,63 @@ package com.example.mycityapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.mycityapp.R
 
 // Set of Material typography styles to start with
+//Use for Title
+var IBMPlexSans = FontFamily(
+    Font(R.font.ibm_plex_sans_regular),
+    Font(R.font.ibm_plex_sans_medium, FontWeight.Medium),
+    Font(R.font.ibm_plex_sans_bold, FontWeight.Bold)
+)
+
+//Use for Main Text
+var Hind = FontFamily(
+    Font(R.font.hind_regular),
+    Font(R.font.hind_medium, FontWeight.Medium),
+    Font(R.font.hind_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = IBMPlexSans,
+        fontWeight = FontWeight.Black,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    headlineLarge = TextStyle(
+        fontFamily =  IBMPlexSans,
+        fontWeight =  FontWeight.Normal,
+        fontSize = 32.sp,
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Hind,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    displayMedium = TextStyle(
+        fontFamily = IBMPlexSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Hind,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Hind,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
