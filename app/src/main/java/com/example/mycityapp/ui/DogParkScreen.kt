df6d9a476428338f25674ec3bbcb8868ecca3043
Keynotseen89 <ksintora@yahoo.com>
2023-11-12 21:02:19 -0800

    Refactored to include Animation fadeout to lazy column

--- app/src/main/java/com/example/mycityapp/ui/DogParkScreen.kt
+++ app/src/main/java/com/example/mycityapp/ui/DogParkScreen.kt
@@ -1,6 +1,15 @@
 package com.example.mycityapp.ui
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring.DampingRatioLowBouncy
+import androidx.compose.animation.core.Spring.StiffnessVeryLow
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -14,7 +23,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -25,6 +34,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,7 +50,7 @@ import com.example.mycityapp.R
 import com.example.mycityapp.data.DogParkDataProvider
 import com.example.mycityapp.model.Recommendation
 import com.example.mycityapp.ui.theme.MyCityAppTheme
-
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun DogParkList(
     recommended: List<Recommendation>,
@@ -51,16 +61,40 @@ fun DogParkList(
     BackHandler {
         onBackPressed()
     }
-    LazyColumn(
-        contentPadding = PaddingValues(dimensionResource(R.dimen.padding_medium)),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
-        modifier = modifier
-    ){
-        items(recommended, key = { dogParks -> dogParks.id}) {dogParks ->
-            DogParkListItem(
-                recommended = dogParks,
-                onItemClick = onClick
-            )
+    val visibleState = remember {
+        MutableTransitionState(false).apply {
+            targetState = true
+        }
+    }
+    //Fade in entry animation for the entire list
+    AnimatedVisibility(
+        visibleState = visibleState,
+        enter = fadeIn(
+            animationSpec = spring(dampingRatio = DampingRatioLowBouncy)
+        ),
+        exit = fadeOut()
+    ) {
+        LazyColumn(
+            contentPadding = PaddingValues(dimensionResource(R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            modifier = modifier
+        ) {
+            itemsIndexed(recommended) { index, dogParks ->
+                DogParkListItem(
+                    recommended = dogParks,
+                    onItemClick = onClick,
+                    modifier = Modifier
+                        .animateEnterExit(
+                            enter = slideInVertically(
+                                animationSpec = spring(
+                                    stiffness = StiffnessVeryLow,
+                                    dampingRatio = DampingRatioLowBouncy
+                                ),
+                                initialOffsetY = { it * (index + 1) } // staggered entrance
+                            )
+                        )
+                )
+            }
         }
     }
 }
