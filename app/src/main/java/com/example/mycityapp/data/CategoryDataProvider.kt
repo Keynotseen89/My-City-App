6c211fc4d8de9944630b9157749a89f4fa7a0fd6
Keynotseen89 <ksintora@yahoo.com>
2024-04-13 20:16:29 -0700

    Refactored code and removed images and function call.

--- app/src/main/java/com/example/mycityapp/data/CategoryDataProvider.kt
+++ app/src/main/java/com/example/mycityapp/data/CategoryDataProvider.kt
@@ -7,36 +7,26 @@ import com.example.mycityapp.R
  * Category Data
  */
 object CategoryDataProvider {
-    val defaultCategory = getCategory()[0]
-    fun getCategory(): List<Categories> {
-
-        return listOf(
-            Categories(
-                id = 1,
-                titleResourceId = R.string.coffee_shops,
-                imageResourceId = R.drawable.coffee_category,
-            ),
-            Categories(
-                id = 2,
-                titleResourceId = R.string.restaurants,
-                imageResourceId = R.drawable.restaurants_cetgory_image,
-            ),
-            Categories(
-                id = 3,
-                titleResourceId = R.string.dog_friendly,
-                imageResourceId = R.drawable.dog_friendly_category,
-            ),
-            Categories(
-                id = 4,
-                titleResourceId = R.string.dog_parks,
-                imageResourceId = R.drawable.dog_park_category,
-            ),
-            Categories(
-                id = 5,
-                titleResourceId = R.string.shopping_center,
-                imageResourceId = R.drawable.shopping_center_category
-            )
-
+    val getCategory = listOf(
+        Categories(
+            id = 1,
+            titleResourceId = R.string.coffee_shops,
+        ),
+        Categories(
+            id = 2,
+            titleResourceId = R.string.restaurants,
+        ),
+        Categories(
+            id = 3,
+            titleResourceId = R.string.dog_friendly,
+        ),
+        Categories(
+            id = 4,
+            titleResourceId = R.string.dog_parks,
+        ),
+        Categories(
+            id = 5,
+            titleResourceId = R.string.shopping_center,
         )
-    }
+    )
 }
