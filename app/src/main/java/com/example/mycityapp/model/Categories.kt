4b46f8c708244927c2a217681ebd6a1e79b29fe6
Keynotseen89 <ksintora@yahoo.com>
2024-04-13 20:18:07 -0700

    Refactored Categories class and removed imageResourceId

--- app/src/main/java/com/example/mycityapp/model/Categories.kt
+++ app/src/main/java/com/example/mycityapp/model/Categories.kt
@@ -1,10 +1,8 @@
 package com.example.mycityapp.model
 
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
 data class Categories(
     val id: Int,
-    @StringRes val titleResourceId: Int,
-    @DrawableRes val imageResourceId: Int
+    @StringRes val titleResourceId: Int
 )
